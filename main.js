7a6b689012366c5fbdafb2cbc05ad64c2b651287
MhdJavithF <javithmd001@gmail.com>
2024-08-19 00:43:47 +0530

    local storage

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ const titleRef = document.querySelector(".task-title");
 const detailRef = document.querySelector(".task-detail");
 const actionRef = document.querySelector('.new-task .action');
 const addRef = document.querySelectorAll('section .catagory .add');
+const taskareaDelRef = document.querySelector('section');
+const clearArrayRef = document.querySelector('.action .delete');
 
 let targetPlace;
+
+window.alert('Please clear older cookies by clicking Delete icon!');
 
 createBtn.addEventListener('click', function(e) {
     targetPlace = document;
     toggle();
 }); 
-
 
 
 addRef.forEach((add) =>{
@@ -33,8 +36,6 @@ function toggle() {
         console.log("hidden");
     }
 };
-
-const tasks = [];
 
 detailRef.addEventListener('keypress', function(e) {
     if(e.key == "Enter"){
@@ -76,27 +77,68 @@ actionRef.addEventListener('click', (e) => {
     }
 });
 
-function createTask(){
-    const idRef = Math.random();
+const tasks = JSON.parse(localStorage.getItem('tasks') || []);
+
+function addDataInStorage(newTask){
+    tasks.push(newTask);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+clearArrayRef.addEventListener('click', (e) => {
+    // console.log("Cleared array elements");
+    tasks.splice(0,tasks.length);
+    alert('Datas were cleared..!')
+});
+
+function createTask() {
+    let idRef = Math.floor(Math.random() * 1000);
     const contentRef = detailRef.value;
     const taskRef = document.createElement('div');
     taskRef.className = 'wrapper-task';
+    taskRef.dataset.id = idRef;     
     taskRef.innerHTML = `
         <div class="id">ID: ${idRef}</div>
         <div class="title">Title: ${titleRef.value}</div>
-        <div class="detail"> Details: ${contentRef}</div>
+        <div class="detail">Details: ${contentRef}</div>
         <div class="delete">
-            <i class="fa-solid fa-trash-can"></i>
+            <i class="fa-solid fa-trash-can" data-id="${idRef}"></i> <!-- Store the ID in the trash icon -->
         </div>
-    `
+    `;
+
     const taskareaRef = targetPlace.querySelector('.taskarea');
+    const newTask = {
+                        id: idRef,
+                        element: taskRef 
+                    };                      // Store the task object in the array
+        
     taskareaRef.appendChild(taskRef);
-    const deleteRef = taskRef.querySelector('.delete');
-    deleteRef.addEventListener('click',(e) => {
-        const selectedTaskRef = e.target.closest('.wrapper-task');
-        console.log(selectedTaskRef);
-        selectedTaskRef.classList.add('hide');
-    });    
+
+    addDataInStorage(newTask);
+
+    // Clear input fields
     titleRef.value = '';
-    detailRef.value = ''; 
-};
+    detailRef.value = '';
+
+    // console.log(tasks);
+}
+
+
+function deleteData(taskId) {
+    const taskIndex = tasks.findIndex((task) => task.id === parseInt(taskId));
+    if (taskIndex > -1) {
+        tasks.splice(taskIndex, 1);
+        // console.log(tasks);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+}
+
+taskareaDelRef.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-trash-can')) {
+        const taskId = e.target.dataset.id;
+        const currTaskRef = e.target.closest('.wrapper-task');
+        if(currTaskRef){
+            currTaskRef.remove();
+        }
+        deleteData(taskId);
+    }
+});
